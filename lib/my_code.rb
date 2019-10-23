e9f2c3a50b593c118b62b8ad5e588493ea15c736
Jen Lindner <jenlindner22@gmail.com>
2019-10-23 23:14:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,9 @@
 def map(array)
-array.map{ |n| n*2 }
-#array.each {|n|}
+ array.map{ |n| n*2 }
+ yield
+array.each {|n|}
 #array.map{ |n| n*n }
 #array.map{ |n| n*-1 }
-  # end
 end  
 
   #[array].map { |n| n * n }
