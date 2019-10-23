787db17a4255a2fcba73a83efef7977a57b61a0f
Jen Lindner <jenlindner22@gmail.com>
2019-10-23 21:54:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,6 @@
 def my_own_map(array, starting_point = 0)
   #[array].map { |n| n * n }
-  answer = source_array.map { |n| n > 0 ? -n : -n } 
+  answer = array.map { |n| n > 0 ? -n : -n } 
   return answer 
   end
   
