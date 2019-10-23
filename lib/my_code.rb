c500009f73a694b6c31772eef38e1c3a2a030296
Jen Lindner <jenlindner22@gmail.com>
2019-10-23 21:20:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,41 @@
-# Your Code Here
+def my_own_map(array, starting_point = 0)
+  [array].map { |n| n * n }
+end
+  
+  
+ #map 
+  #returns array with negative values 
+#   original values (.each)
+#   values multipled by 2 
+#   values squared
+  
+# reduce
+#   returns running total without starting point 
+#   returns running total with starting point
+#   returns true when all values are truthy
+#   returns false when any value is false
+#   returns true when a truthy value is present
+#   returns false when no truthy value is present
+  
+  
+#   def map_to_negativize(source_array)
+#   new = []
+#   i = 0
+#   while i < source_array.length do
+#     new.push( source_array[i] * -1 ) # <== Unique work
+#     i += 1
+#   end
+#   return new
+# end
+# end
+
+# def map_to_negativize(source_array)
+# count = 0
+# negative = -1
+# while count < source_array.length do
+# source_array[count] = source_array[count] * negative
+# source_array[count]
+# count += 1
+# end
+# source_array
+# end
