95aa73ac46ab6cb464aa348aaea9052b6de6e0c6
Jen Lindner <jenlindner22@gmail.com>
2019-10-24 00:53:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,10 @@ def map(array)
 
   
 def reduce(array, starting_point = 0)
-  array.reduce{|x,y| yield x,y}
+  if starting_point != 0 
+    array.reduce(starting_point){|x,y| yield x,y} 
+  else 
+    array.reduce{|x,y| yield x,y}
 end
 #   returns running total without starting point 
 #   returns running total with starting point
