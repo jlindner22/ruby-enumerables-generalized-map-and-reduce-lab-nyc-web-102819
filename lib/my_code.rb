8bb4b305a2412de19b150b419079a382d8c5d314
Jen Lindner <jenlindner22@gmail.com>
2019-10-24 00:57:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,5 @@
 def map(array)
-  new = []
- for element in array 
- new.push yield(element)
- end
- new
+array.map{|n| yield n}
  end
 
   
@@ -14,11 +10,5 @@ def reduce(array, starting_point = 0)
     array.reduce{|x,y| yield x,y}
   end
 end
-#   returns running total without starting point 
-#   returns running total with starting point
-#   returns true when all values are truthy
-#   returns false when any value is false
-#   returns true when a truthy value is present
-#   returns false when no truthy value is present
-  
+
   
